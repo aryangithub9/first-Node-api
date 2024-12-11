a9bd4920d3494d3f818ff81227f7139805b7b0d2
Aryan <aryansinghdhampur@gmail.com>
2024-12-11 19:04:48 +0530

    add an another route /name

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express';
 const PORT =9000;
 const app = express();
 
+app.use(express.json())
+
 app.get('/home',(req,res)=>{
     res.send('You are on home page');
     console.log(`Someone Reaches on Server from this ip address${req.ip}`)
@@ -13,6 +15,12 @@ app.get('/',(req,res)=>{
 
 app.get('/about',(req,res)=>{
     res.send('You are on About Page')
+})
+
+app.post('/name', (req,res)=>{
+    const {name} = req.body;
+    const Cname = name.toUpperCase();
+    return res.json({Cname});
 })
 
 
